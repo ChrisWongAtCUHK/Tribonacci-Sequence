aab33184cf8a4526863dc69869caa7152ec58554
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-30 09:20:12 +0800

    lodimob java solution.

--- java/Xbonacci.java
+++ java/Xbonacci.java
@@ -1,31 +1,13 @@
+import java.util.Arrays;
+
 public class Xbonacci {
 
 	public double[] tribonacci(double[] s, int n) {
-		// if n==0, then return an empty array
-	    if(n == 0) return new double[0];
-
-		// the list of result
-		double[] result = new double[n];
-		
-		// special cases
-		if(n < s.length){
-			for(int i = 0; i < n; i++){
-				result[i] = s[i];
-			}
-			
-			return result;
+		// hackonacci me
+		double[] tri = Arrays.copyOf(s, n);
+		for (int i = s.length; i < n; i++) {
+			tri[i] = tri[i - 1] + tri[i - 2] + tri[i - 3];
 		}
-		
-		// normal cases
-		for(int i = 0; i < s.length; i++){
-			result[i] = s[i];
-		}
-		
-		// calculation
-		for(int i = s.length; i <= n - 1; i++){
-			result[i] = result[i - 3] + result[i - 2] + result[i - 1];
-		}
-		
-		return result;
+		return tri;
 	}
 }
